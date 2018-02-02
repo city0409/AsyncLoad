79af44a35d1ef2765f63712c8892f5d531651154
city0409 <250924125@qq.com>
2018-02-02 15:49:09 +0800

    Initial commit

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour 
+{
+
+	private void Awake() 
+	{
+		
+	}
+	
+	private void Update () 
+	{
+        if (Input .GetKeyDown (KeyCode .L))
+        {
+            StartCoroutine(LoadNewGame());
+        }
+	}
+
+    private IEnumerator LoadNewGame()
+    {
+        print("LoadNewGame");
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync(1,LoadSceneMode.Single);
+        ao.allowSceneActivation = false;
+
+        while (ao .progress <0.9f)
+        {
+            yield return null;
+        }
+        //此处有黑屏过渡
+        ao.allowSceneActivation = true;
+        print("222");
+
+    }
+}
---
+++ Assets/RotateSelf.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSelf : MonoBehaviour 
+{
+
+	private void Awake() 
+	{
+		
+	}
+	
+	private void Update () 
+	{
+        transform.Rotate(new Vector3(0, 0, Time.deltaTime * 10));
+	}
+}
